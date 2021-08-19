9e010a6a2136713997dd362ffb1b909ab66d12b5
Chris <chris.ware.dev@gmail.com>
2021-08-18 23:04:48 -0700

    Search Input with Styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,27 @@
 import Layout from "../components/Layout/Layout";
+import SearchInput from "../components/SearchInput/SearchInput";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ countries }) {
+  console.log(countries);
   return (
     <>
       <Layout>
-        Main
+        <div className={styles.counts}>Found {countries.length} countries</div>
+        
+        <SearchInput placeholder='Filter by Name, Reigon, or Subregion' />
       </Layout>
     </>
-  )
+  );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries = await res.json();
+
+  return {
+    props: {
+      countries,
+    },
+  };
+};
